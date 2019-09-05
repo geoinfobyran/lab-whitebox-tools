0800affa4fc42b3b67d0c40f6802ec4cfa960567
johnniehard <johnnie@synvinkel.org>
2019-09-05 09:50:32 +0200

    init

---
+++ getwhitebox.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://jblindsay.github.io/ghrg/WhiteboxTools/WhiteboxTools_linux_amd64.tar.xz
+
+tar -xvf WhiteboxTools_linux_amd64.tar.xz
+
+rm WhiteboxTools_linux_amd64.tar.xz
