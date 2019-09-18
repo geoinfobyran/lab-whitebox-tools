4580ca0f9a22f65062f4bae58c242799c89cfdd4
johnniehard <johnnie@synvinkel.org>
2019-09-18 11:41:43 +0200

    testing different tools

--- basins.py
+++ basins.py
@@ -6,19 +6,18 @@ from WBT.whitebox_tools import WhiteboxTools
 wbt = WhiteboxTools()
 
 work_dir = os.path.dirname(os.path.abspath(__file__))
-wb_dir = work_dir + "/WBT/"
-data_dir = work_dir + "/data/"
-out_dir = work_dir + "/out/"
+wb_dir = work_dir + "/WBT"
+data_dir = work_dir + "/data"
+out_dir = work_dir + "/out"
 
 wbt.set_whitebox_dir(wb_dir)
 
 omr = '925'
 
 def run():
-    outfile = f'{out_dir}/streams/{omr}.tif'
-    wbt.greater_than(
-        f'{data_dir}/D8_Flowacc/{omr}.dep',
-        '10000',
+    outfile = f'{out_dir}/basins/{omr}.tif'
+    wbt.basins(
+        f'{data_dir}/D8_Pointer/{omr}.dep',
         outfile
     )
     add_srs=f'gdal_edit.py -a_srs EPSG:3006 {outfile}'
@@ -29,4 +28,3 @@ start = time.time()
 run()
 end = time.time()
 print(end - start)
-
--- strahlerorderbasins.py
+++ strahlerorderbasins.py
@@ -6,19 +6,19 @@ from WBT.whitebox_tools import WhiteboxTools
 wbt = WhiteboxTools()
 
 work_dir = os.path.dirname(os.path.abspath(__file__))
-wb_dir = work_dir + "/WBT/"
-data_dir = work_dir + "/data/"
-out_dir = work_dir + "/out/"
+wb_dir = work_dir + "/WBT"
+data_dir = work_dir + "/data"
+out_dir = work_dir + "/out"
 
 wbt.set_whitebox_dir(wb_dir)
 
 omr = '925'
 
 def run():
-    outfile = f'{out_dir}/streams/{omr}.tif'
-    wbt.greater_than(
-        f'{data_dir}/D8_Flowacc/{omr}.dep',
-        '10000',
+    outfile = f'{out_dir}/strahlerorderbasins/{omr}.tif'
+    wbt.strahler_order_basins(
+        f'{data_dir}/D8_Pointer/{omr}.dep',
+        f'{out_dir}/streams/{omr}.dep',
         outfile
     )
     add_srs=f'gdal_edit.py -a_srs EPSG:3006 {outfile}'
